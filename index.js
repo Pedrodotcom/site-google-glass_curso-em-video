9f246175611a200d649bd5709ec037e4664380fa
Pedrodotcom <pedroluisaantonio@gmail.com>
2020-05-13 17:12:05 -0300

    Nunjucks setted

--- index.js
+++ index.js
@@ -10,42 +10,60 @@ nunjucks.configure('pages', {
 
 // Rotas da aplicação
 
-app.get('/', function(req, res) {
-    res.render('index.html')
-})
+    app.get('/', function(req, res) {
+        res.render('index.html')
+    })
 
+    app.get('/specs', function(req, res) {
+        res.render('specs.html')
+    })
+
+    app.get('/google-glass', function(req, res) {
+        res.render('google-glass.html')
+    })
+
+    app.get('/fotos', function(req, res) {
+        res.render('fotos.html')
+    })
+
+    app.get('/multimidia', function(req, res) {
+        res.render('multimidia.html')
+    })
+
+    app.get('/fale-conosco', function(req, res) {
+        res.render('fale-conosco.html')
+    })
+
+    app.post('/fale-conosco/submit', function(req, res) {
+    
+            if(err) {
+                return res.status(400).send('Server error. Please, try again.')
+            }
+
+            res.send("Mensagem registrada com sucesso!")
+            res.redirect('/')
+    })
+
+// Porta do servidor
+
+    app.listen(8000, function() {
+        console.log('The server is running in port 8000')
+    })
+
+/*
 app.get('/specs', function(req, res) {
-    res.render('specs.html')
-})
-
-app.get('/google-glass', function(req, res) {
-    res.render('google-glass.html')
-})
-
-app.get('/fotos', function(req, res) {
-    res.render('fotos.html')
-})
-
-app.get('/multimidia', function(req, res) {
-    res.render('multimidia.html')
-})
-
-app.get('/fale-conosco', function(req, res) {
-    res.render('fale-conosco.html')
-})
-
-app.post('/fale-conosco/submit', function(req, res) {
-  
+    res.sendFile(__dirname + '/pages/specs.html', function(err) {
         if(err) {
             return res.status(400).send('Server error. Please, try again.')
         }
-
-        res.send("Mensagem registrada com sucesso!")
-        res.redirect('/')
+    })
 })
 
 
-app.listen(8000, function() {
-    console.log('The server is running in port 8000')
-})
+, function(err) {
+    if(err) {
+        return res.status(400).send('Server error. Please, try again.')
+    }
+}
 
+*/
